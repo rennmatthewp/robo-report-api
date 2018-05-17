076163cb035f014ea44180d2090490d9eb29345c
Matthew Renn <rennmatthewp@gmail.com>
2018-05-16 23:00:48 -0600

    add patch user by id test add delete user by id route and test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,7 +17,7 @@ const requiredUserParameters = [
   'address',
   'city',
   'state',
-  'zipcode',
+  'zipcode'
 ];
 
 const requiredComplaintParameters = [
@@ -33,25 +33,29 @@ const requiredComplaintParameters = [
   'altPhone',
   'permissionGranted',
   'businessName',
-  'agentName',
+  'agentName'
 ];
 
 router.get('/users', (request, response) => {
-  database('users').select()
-    .then((users) => {
+  database('users')
+    .select()
+    .then(users => {
       response.status(200).json(users);
     })
-    .catch((error) => {
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
 
 router.get('/users/:id', (request, response) => {
   const { id } = request.params;
-  database('users').where('id', id)
-    .then((user) => {
+  database('users')
+    .where('id', id)
+    .then(user => {
       if (!user[0]) {
-        return response.status(404).json({ error: `Could not find user with id=${id}.` });
+        return response
+          .status(404)
+          .json({ error: `Could not find user with id: ${id}.` });
       }
       return response.status(200).json(user[0]);
     })
@@ -64,7 +68,7 @@ router.post('/users', (request, response) => {
   for (const parameter of requiredUserParameters) {
     if (!user[parameter]) {
       return response.status(422).json({
-        error: `Expected format: {firstName: <String>, lastName: <String>, email: <String>, phone: <String>, phoneType: <String>, address: <String>, city: <String>, state: <String>, zipcode: <String>}. Missing required property ${parameter}.`,
+        error: `Expected format: {firstName: <String>, lastName: <String>, email: <String>, phone: <String>, phoneType: <String>, address: <String>, city: <String>, state: <String>, zipcode: <String>}. Missing required property ${parameter}.`
       });
     }
   }
@@ -79,38 +83,65 @@ router.patch('/users/:id', (request, response) => {
   const { id } = request.params;
   const revision = request.body;
   let correctFormat = true;
-  Object.keys(revision).forEach((key) => {
+  Object.keys(revision).forEach(key => {
     if (!requiredUserParameters.includes(key)) {
       correctFormat = false;
     }
   });
   if (!correctFormat) {
-    return response.status(422).json({ error: 'Cannot update user, invalid property provided. Valid properties include: {firstName: <String>, lastName: <String>, email: <String>, phone: <String>, phoneType: <String>, address: <String>, city: <String>, state: <String>, zipcode: <String>}' });
+    return response.status(422).json({
+      error:
+        'Cannot update user, invalid property provided. Valid properties include: {firstName: <String>, lastName: <String>, email: <String>, phone: <String>, phoneType: <String>, address: <String>, city: <String>, state: <String>, zipcode: <String>}'
+    });
   }
 
-  return database('users').where('id', id)
+  return database('users')
+    .where('id', id)
     .select()
     .update(revision)
-    .then(() => response.status(201).json({ message: `updated user with id=${id}` }))
+    .then(() =>
+      response.status(201).json({ message: `updated user with id: ${id}` })
+    )
     .catch(error => response.status(500).json({ error }));
 });
 
+router.delete('/users/:id', (request, response) => {
+  const { id } = request.params;
+  database('complaints')
+    .where('user_id', id)
+    .del()
+    .then(() => {
+      database('users')
+        .where('id', id)
+        .del()
+        .then(user => {
+          return response.status(200).json(user);
+        })
+        .catch(error => response.status(500).json(error));
+    })
+    .catch(error => response.status(500).json(error));
+});
+
 router.get('/complaints', (request, response) => {
-  database('complaints').select()
-    .then((complaints) => {
+  database('complaints')
+    .select()
+    .then(complaints => {
       response.status(200).json(complaints);
     })
-    .catch((error) => {
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
 
 router.get('/complaints/:id', (request, response) => {
   const { id } = request.params;
-  database('complaints').where('id', id)
-    .then((complaint) => {
+  database('complaints')
+    .where('id', id)
+    .then(complaint => {
       if (!complaint[0]) {
-        return response.status(404).json({ error: `Could not find complaint with id=${id}.` });
+        return response
+          .status(404)
+          .json({ error: `Could not find complaint with id: ${id}.` });
       }
       return response.status(200).json(complaint[0]);
     })
@@ -123,7 +154,7 @@ router.post('/complaints', (request, response) => {
   for (const parameter of requiredComplaintParameters) {
     if (complaint[parameter] === undefined) {
       return response.status(422).json({
-        error: `Expected format: {user_id: <Integer>, isSoliciting: <String>, subject: <String>, description: <String>, callerIdNumber: <String>, callerIdName: <String>, date: <String>, time: <String>, type: <String>, altPhone: <String>, permissionGranted: <Boolean>, businessName: <String>, agentName: <String>}. Missing required property ${parameter}.`,
+        error: `Expected format: {user_id: <Integer>, isSoliciting: <String>, subject: <String>, description: <String>, callerIdNumber: <String>, callerIdName: <String>, date: <String>, time: <String>, type: <String>, altPhone: <String>, permissionGranted: <Boolean>, businessName: <String>, agentName: <String>}. Missing required property ${parameter}.`
       });
     }
   }
@@ -138,36 +169,50 @@ router.patch('/complaints/:id', (request, response) => {
   const { id } = request.params;
   const revision = request.body;
   let correctFormat = true;
-  Object.keys(revision).forEach((key) => {
+  Object.keys(revision).forEach(key => {
     if (!requiredComplaintParameters.includes(key)) {
       correctFormat = false;
     }
   });
   if (!correctFormat) {
-    return response.status(422).json({ error: 'Cannot update complaint, invalid property provided. Valid properties include: {user_id: <Integer>, isSoliciting: <String>, subject: <String>, description: <String>, callerIdNumber: <String>, callerIdName: <String>, date: <String>, time: <String>, type: <String>, altPhone: <String>, permissionGranted: <Boolean>, businessName: <String>, agentName: <String>}' });
+    return response.status(422).json({
+      error:
+        'Cannot update complaint, invalid property provided. Valid properties include: {user_id: <Integer>, isSoliciting: <String>, subject: <String>, description: <String>, callerIdNumber: <String>, callerIdName: <String>, date: <String>, time: <String>, type: <String>, altPhone: <String>, permissionGranted: <Boolean>, businessName: <String>, agentName: <String>}'
+    });
   }
 
-  return database('complaints').where('id', id)
+  return database('complaints')
+    .where('id', id)
     .select()
     .update(revision)
-    .then((updateCount) => {
+    .then(updateCount => {
       if (updateCount === 0) {
-        return response.status(422).json({ error: `${updateCount} column(s) updated. Unable to find complaint with id=${id}` });
+        return response.status(422).json({
+          error: `${updateCount} column(s) updated. Unable to find complaint with id=${id}`
+        });
       }
       const updates = Object.keys(revision).join(', ');
-      return response.status(201).json({ message: `${updateCount} column(s) updated: [ ${updates} ]. Complaint id=${id}.` });
+      return response.status(201).json({
+        message: `${updateCount} column(s) updated: [ ${updates} ]. Complaint id=${id}.`
+      });
     })
     .catch(error => response.status(500).json({ error }));
 });
 
 router.delete('/complaints/:id', (request, response) => {
   const { id } = request.params;
-  database('complaints').where('id', id).del()
-    .then((deleteCount) => {
+  database('complaints')
+    .where('id', id)
+    .del()
+    .then(deleteCount => {
       if (deleteCount === 0) {
-        return response.status(422).json({ error: `${deleteCount} row(s) deleted. No complaint found with id=${id}` });
+        return response.status(422).json({
+          error: `${deleteCount} row(s) deleted. No complaint found with id=${id}`
+        });
       }
-      return response.status(200).json({ message: `${deleteCount} row(s) deleted. Deleted complaint with id=${id}.` });
+      return response.status(200).json({
+        message: `${deleteCount} row(s) deleted. Deleted complaint with id=${id}.`
+      });
     })
     .catch(error => response.status(500).json({ error }));
 });
